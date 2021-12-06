8bf2ddde88e4704fff25aaabd00e251811479897
kim-jaun <67224239+kim-jaun@users.noreply.github.com>
2021-12-06 09:49:11 +0900

    commit

---
+++ spring-boot-of-vs-code/src/main/kotlin/com/ch/hello/springbootofvscode/SpringBootOfVsCodeApplication.kt
@@ -0,0 +1,11 @@
+package com.ch.hello.springbootofvscode
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootOfVsCodeApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringBootOfVsCodeApplication>(*args)
+}
